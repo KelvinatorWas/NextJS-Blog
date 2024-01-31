42ff05b72dad1182d17ba9fd16f597fc75ed5698
KelvinatorWas <liepkalnskalvis@gmail.com>
2024-01-31 23:34:23 +0200

    added new path

--- src/components/CustomLink/customLink.tsx
+++ src/components/CustomLink/customLink.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { joinClass } from "@/utils/stringManiplation";
+import { joinClass } from "$/utils/stringManiplation";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
